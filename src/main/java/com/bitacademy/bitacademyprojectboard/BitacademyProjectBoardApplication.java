f0e6137592a050ff1aaee00303404603dd373834
june <june@localhost>
2023-01-26 15:12:26 +0900

    first commit

---
+++ src/main/java/com/bitacademy/bitacademyprojectboard/BitacademyProjectBoardApplication.java
@@ -0,0 +1,13 @@
+package com.bitacademy.bitacademyprojectboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BitacademyProjectBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BitacademyProjectBoardApplication.class, args);
+	}
+
+}
