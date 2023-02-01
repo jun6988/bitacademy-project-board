1671b4fc5e0e4e904c3b1b9bda07d7679ed8b755
june <june@localhost>
2023-02-01 16:17:12 +0900

    API를 간편하게 만들기 위해 스프링 부트에 기능을 추가 게시글, 댓글의 jason api를 자동으로 restful 하게 만들게끔 설정

--- src/main/java/com/bitacademy/projectboard/repository/ArticleRepository.java
+++ src/main/java/com/bitacademy/projectboard/repository/ArticleRepository.java
@@ -1,9 +1,11 @@
 package com.bitacademy.projectboard.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import com.bitacademy.projectboard.domain.Article;
 
+@RepositoryRestResource
 public interface ArticleRepository extends JpaRepository<Article, Long> {
 
 }
--- src/main/java/com/bitacademy/projectboard/repository/Article_CommentRepository.java
+++ src/main/java/com/bitacademy/projectboard/repository/Article_CommentRepository.java
@@ -1,9 +1,11 @@
 package com.bitacademy.projectboard.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import com.bitacademy.projectboard.domain.Article_Comment;
 
+@RepositoryRestResource
 public interface Article_CommentRepository extends JpaRepository<Article_Comment, Long> {
 
 }
