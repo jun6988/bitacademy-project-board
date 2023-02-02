e00ea7f87629437382892ff016b700ebc4dc1380
june <june@218.38.137.27>
2023-02-02 21:57:09 +0900

    QuerydslPredicateExecutor & QuerydslBinderCustomizer 이용

--- src/main/java/com/bitacademy/projectboard/repository/ArticleRepository.java
+++ src/main/java/com/bitacademy/projectboard/repository/ArticleRepository.java
@@ -1,11 +1,30 @@
 package com.bitacademy.projectboard.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+import org.springframework.data.querydsl.binding.QuerydslBinderCustomizer;
+import org.springframework.data.querydsl.binding.QuerydslBindings;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import com.bitacademy.projectboard.domain.Article;
+import com.bitacademy.projectboard.domain.QArticle;
+import com.querydsl.core.types.dsl.DateTimeExpression;
+import com.querydsl.core.types.dsl.StringExpression;
 
 @RepositoryRestResource
-public interface ArticleRepository extends JpaRepository<Article, Long> {
+public interface ArticleRepository extends 
+	JpaRepository<Article, Long>,
+	QuerydslPredicateExecutor<Article>, // Article 안에 있는 모든 기본 검색 기능 
+	QuerydslBinderCustomizer<QArticle> {
 
+	@Override
+	default void customize(QuerydslBindings bindings, QArticle root) {
+		bindings.excludeUnlistedProperties(true);
+		bindings.including(root.title, root.content, root.hashtag, root.createdAt, root.createdBy);
+        bindings.bind(root.title).first(StringExpression::containsIgnoreCase);
+        bindings.bind(root.content).first(StringExpression::containsIgnoreCase);
+        bindings.bind(root.hashtag).first(StringExpression::containsIgnoreCase);
+        bindings.bind(root.createdAt).first(DateTimeExpression::eq);
+        bindings.bind(root.createdBy).first(StringExpression::containsIgnoreCase);
+	} 
 }
--- src/main/java/com/bitacademy/projectboard/repository/Article_CommentRepository.java
+++ src/main/java/com/bitacademy/projectboard/repository/Article_CommentRepository.java
@@ -1,11 +1,29 @@
 package com.bitacademy.projectboard.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+import org.springframework.data.querydsl.binding.QuerydslBinderCustomizer;
+import org.springframework.data.querydsl.binding.QuerydslBindings;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import com.bitacademy.projectboard.domain.Article_Comment;
+import com.bitacademy.projectboard.domain.QArticle_Comment;
+import com.querydsl.core.types.dsl.DateTimeExpression;
+import com.querydsl.core.types.dsl.StringExpression;
 
 @RepositoryRestResource
-public interface Article_CommentRepository extends JpaRepository<Article_Comment, Long> {
-
+public interface Article_CommentRepository extends 
+	JpaRepository<Article_Comment, Long>, 
+	QuerydslPredicateExecutor<Article_Comment>, 
+	QuerydslBinderCustomizer<QArticle_Comment> {
+		
+	@Override
+	default void customize(QuerydslBindings bindings, QArticle_Comment root) {
+		bindings.excludeUnlistedProperties(true);
+		bindings.including(root.content, root.createdAt, root.createdBy);
+	    bindings.bind(root.content).first(StringExpression::containsIgnoreCase);
+	    bindings.bind(root.createdAt).first(DateTimeExpression::eq);
+	    bindings.bind(root.createdBy).first(StringExpression::containsIgnoreCase);
+	} 	
 }
+
