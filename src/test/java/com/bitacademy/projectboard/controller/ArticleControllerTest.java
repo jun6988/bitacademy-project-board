f168fee061e23c91e3a565fb39d6f8abb88cb346
june <june@218.38.137.27>
2023-02-03 16:42:41 +0900

    컨트롤러 핸들러 메소드로 테스트 통과 뷰 뼈대 테스트 : media type 검사에서 부가적인 옵션이 들어갈 수 있음을 확인

--- src/test/java/com/bitacademy/projectboard/controller/ArticleControllerTest.java
+++ src/test/java/com/bitacademy/projectboard/controller/ArticleControllerTest.java
@@ -32,7 +32,7 @@ class ArticleControllerTest {
 		// When & Then
 		mvc.perform(MockMvcRequestBuilders.get("/articles"))
 				.andExpect(status().isOk())
-				.andExpect(content().contentType(MediaType.TEXT_HTML))
+				.andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
 				.andExpect(view().name("articles/index"))
 				.andExpect(model().attributeExists("articles"));
 	}
@@ -45,7 +45,7 @@ class ArticleControllerTest {
 		// When & Then
 		mvc.perform(MockMvcRequestBuilders.get("/articles/1"))
 				.andExpect(status().isOk())
-				.andExpect(content().contentType(MediaType.TEXT_HTML))
+				.andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
 				.andExpect(view().name("articles/detail"))
 				.andExpect(model().attributeExists("article"))
 				.andExpect(model().attributeExists("article_Comments"));
@@ -59,7 +59,7 @@ class ArticleControllerTest {
 		// When & Then
 		mvc.perform(MockMvcRequestBuilders.get("/articles/search"))
 				.andExpect(status().isOk())
-				.andExpect(content().contentType(MediaType.TEXT_HTML))
+				.andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
 				.andExpect(model().attributeExists("articles/search"));
 	}
 	
@@ -71,7 +71,7 @@ class ArticleControllerTest {
 		// When & Then
 		mvc.perform(MockMvcRequestBuilders.get("/articles/search-hashtag"))
 				.andExpect(status().isOk())
-				.andExpect(content().contentType(MediaType.TEXT_HTML))
+				.andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
 				.andExpect(model().attributeExists("articles/search-hashtag"));
 	}
 }
