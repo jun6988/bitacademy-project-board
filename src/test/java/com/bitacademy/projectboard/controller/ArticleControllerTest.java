4b4d3dd1c47a636b48d651b0b3287ef35dd56d70
june <june@localhost>
2023-02-06 11:58:52 +0900

    Test단계, 아직 인증 기능 구현 단계가 아니므로 모든 인증 설정을 오픈한다. 폼 로그인을 활성화해서 로그인 뷰가 그려지게 함.

--- src/test/java/com/bitacademy/projectboard/controller/ArticleControllerTest.java
+++ src/test/java/com/bitacademy/projectboard/controller/ArticleControllerTest.java
@@ -1,20 +1,25 @@
 package com.bitacademy.projectboard.controller;
 
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+import com.bitacademy.projectboard.config.SecurityConfig;
 
 
 
 @DisplayName("View 컨트롤러 - 게시글")
+@Import(SecurityConfig.class)
 @WebMvcTest(ArticleController.class)
 class ArticleControllerTest {
 
